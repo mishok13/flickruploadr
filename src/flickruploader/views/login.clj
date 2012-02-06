b0d1e4613e95b63f1b7a29d46c44cca3a8f60133
Andrii Mishkovskyi <gdmfsob@mishkovskyi.net>
2012-02-06 20:42:14 +0100

    Playing around with noir

--- src/flickruploader/views/login.clj
+++ src/flickruploader/views/login.clj
@@ -1,7 +1,12 @@
 (ns flickruploader.views.login
+  (:require noir.response)
   (:use [noir.core :only [defpage]]
         [hiccup.core :only [html]]))
 
 
 (defpage "/" []
   "It works!")
+
+
+(defpage "/json/:key" {key :key}
+  (noir.response/json {key 42}))
