8081fe2c799a0b8b736407101f8797037f7aa1ce
Andrii Mishkovskyi <gdmfsob@mishkovskyi.net>
2012-02-06 20:30:48 +0100

    It works!

---
+++ src/flickruploader/views/login.clj
@@ -0,0 +1,7 @@
+(ns flickruploader.views.login
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+
+(defpage "/" []
+  "It works!")
--- src/flickruploader/views/welcome.clj
+++ src/flickruploader/views/welcome.clj
@@ -1,9 +1,10 @@
 (ns flickruploader.views.welcome
-  (:require [flickruploader.views.common :as common]
-            [noir.content.getting-started])
-  (:use [noir.core :only [defpage]]
-        [hiccup.core :only [html]]))
+  ;; (:require [flickruploader.views.common :as common]
+  ;;           [noir.content.getting-started])
+  ;; (:use [noir.core :only [defpage]]
+  ;;       [hiccup.core :only [html]])
+  )
 
-(defpage "/welcome" []
-         (common/layout
-           [:p "Welcome to flickruploader"]))
+;; (defpage "/welcome" []
+;;          (common/layout
+;;            [:p "Welcome to flickruploader"]))
